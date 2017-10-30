4b080d314d74ae2883c8d31246c20c66ebf65200
Wolod <damxirex@gmail.com>
2017-10-30 09:29:15 +0100

    first commit

---
+++ 4/4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Telewizor jeden = new Telewizor("Philips",100,21);
+            Console.Write(jeden.marka);
+            Console.Write("\n");
+            Console.Write(jeden.cena);
+            Console.Write("\n");
+            Console.Write(jeden.ekran);
+            Console.ReadKey();
+        }
+    }
+}
